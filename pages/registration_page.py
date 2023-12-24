5af87a5af2830fc75345061a395a6fdbc84675eb
Stanislav Dudnik <136478699+idudnik@users.noreply.github.com>
2023-12-24 16:10:58 +0200

    high level

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -3,57 +3,102 @@ from pathlib import Path
 from selene import browser, have, be, by
 from selene.support.shared.jquery_style import s
 
+from data.users import User
+
 
 class RegistationPage:
+    def __init__(self):
+
+        self.first_name = browser.element('#firstName')
+        self.last_name = browser.element('#lastName')
+        self.email=browser.element('#userEmail')
+        self.gender=browser.all('[for*=gender-radio]')
+        self.mobile=browser.element('#userNumber')
+        self.date_of_birth = browser.element('#dateOfBirthInput')
+        self.month_of_birth = browser.element('.react-datepicker__month-select')
+        self.year_of_birth = browser.element('.react-datepicker__year-select')
+        self.subject=browser.element('#subjectsInput')
+        self.hobbie=browser.all('.custom-checkbox')
+        self.picture=browser.element('#uploadPicture')
+        self.address=browser.element('#currentAddress')
+        self.state = browser.element('#react-select-3-input')
+        self.city = browser.element('#react-select-4-input')
+
+        self.submit = browser.element('#submit')
+
+
+
+
+        self.registered_user_data = browser.all('tbody tr td:last-child')
+
+
+
+
+
+
+
+
+
     def open(self):
         browser.open("/automation-practice-form")
 
     def fill_first_name(self, value):
-        browser.element('#firstName').should(be.blank)
-        browser.element('#firstName').type(value)
+        self.first_name.should(be.blank)
+        self.first_name.type(value)
 
     def fill_last_name(self, value):
-        browser.element('#lastName').type(value)
+        self.last_name.type(value)
 
     def fill_email(self, mail):
-        browser.element('#userEmail').type(mail)
+        self.email.type(mail)
 
     def fill_gender(self, value):
-        browser.all('[for*=gender-radio]').element_by(have.text(value)).click()
+        self.gender.element_by(have.text(value)).click()
 
     def fill_mobile(self, value):
-        browser.element('#userNumber').type(value)
+        self.mobile.type(value)
 
     def fill_date_of_birth(self, year, month, day):
-        s('#dateOfBirthInput').click()
-        s('.react-datepicker__month-select').click().element(by.text(month)).click()
-        s('.react-datepicker__year-select').click().element(by.text(year)).click()
-        s(f'.react-datepicker__day--0{day}').click()
+        self.date_of_birth.click()
+        self.month_of_birth.type(month)
+        self.year_of_birth.type(year)
+        browser.element(f'.react-datepicker__day--0{day}').click()
 
     def fill_subject(self, value):
-        browser.element('#subjectsInput').type(value).press_enter()
+        self.subject.type(value).press_enter()
 
     def fill_hobbie(self, value):
-        browser.all('.custom-checkbox').element_by(have.exact_text(value)).click()
+
+        self.hobbie.element_by(have.exact_text(value)).should(be.clickable).click()
 
     def picture_upload(self, picture):
         picture_path = Path(__file__).parent.parent.joinpath('resources', picture)
-        browser.element('#uploadPicture').send_keys(str(picture_path))
+        self.picture.send_keys(str(picture_path))
 
     def fill_address(self, value):
-        browser.element('#currentAddress').type(value)
+        self.address.type(value)
 
     def fill_state(self, value):
-        browser.element('#react-select-3-input').type(value).press_enter()
+        self.state.type(value).press_enter()
 
     def fill_city(self, value):
-        browser.element('#react-select-4-input').type(value).press_enter()
+        self.city.type(value).press_enter()
 
     def press_submit(self):
-        browser.element('#submit').press_enter()
+       self.submit.press_enter()
 
-    def should_include_info(self, full_name, email, gender, mobile, date_of_bitrth, subject, hobbie, picture_name,
-                            address, city_and_state):
-        browser.all('tbody tr td:last-child').should(
-            have.exact_texts(full_name, email, gender, mobile, date_of_bitrth, subject, hobbie, picture_name, address,
-                             city_and_state))
+
+    def register(self, user: User):
+        self.fill_first_name(user.first_name)
+        self.fill_last_name(user.last_name)
+        self.fill_email(user.email)
+        self.fill_gender(user.gender)
+        self.fill_mobile(user.mobile)
+        self.fill_date_of_birth(user.day_of_birth, user.month_of_birth, user.year_of_birth)
+        self.fill_subject(user.subject)
+        self.fill_hobbie(user.hobbie)
+        self.picture_upload(user.picture_name)
+        self.fill_address(user.address)
+        self.fill_state(user.state)
+        self.fill_city(user.city)
+        self.press_submit()
--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,30 +1,43 @@
+from data import users
+from data.users import ivan
 from pages.registration_page import RegistationPage
-
+from selene import have
 
 def test_form_submit_checker():
     registration_page = RegistationPage()
+    ivan=users.ivan
+
     registration_page.open()
 
     # WHEN
+    registration_page.register(users.ivan)
 
-    registration_page.fill_first_name('Ivan')
-    registration_page.fill_last_name('Ivanov')
-    registration_page.fill_email('ivanov@mail.com')
-    registration_page.fill_gender('Male')
-    registration_page.fill_mobile('8925239563')
-    registration_page.fill_date_of_birth('1988', 'June', '01')
-    registration_page.fill_subject('English')
-    registration_page.fill_hobbie('Reading')
-    registration_page.picture_upload('1.jpeg')
-    registration_page.fill_address('Leskova street,8')
-    registration_page.fill_state('Uttar Pradesh')
-    registration_page.fill_city('Agra')
+    registration_page.fill_first_name(ivan.first_name)
+    registration_page.fill_last_name(ivan.last_name)
+    registration_page.fill_email(ivan.email)
+    registration_page.fill_gender(ivan.gender)
+    registration_page.fill_mobile(ivan.mobile)
+    registration_page.fill_date_of_birth(ivan.day_of_birth,ivan.month_of_birth ,ivan.year_of_birth)
+    registration_page.fill_subject(ivan.subject)
+    registration_page.fill_hobbie(ivan.hobbie)
+    registration_page.picture_upload(ivan.picture_name)
+    registration_page.fill_address(ivan.address)
+    registration_page.fill_state(ivan.city)
+    registration_page.fill_city(ivan.state)
     registration_page.press_submit()
 
     # THEN
-    registration_page.should_include_info('Ivan Ivanov',
-                                          'ivanov@mail.com', 'Male', '8925239563',
-                                          '01 June,1988',
-                                          'English',
-                                          'Reading', '1.jpeg', 'Leskova street,8',
-                                          'Uttar Pradesh Agra')
+
+    registration_page.registered_user_data.should(have.exact_texts('Ivan Ivanov',
+                                                                   'ivanov@mail.com', 'Male', '8925239563',
+                                                                   '01 June,1988',
+                                                                   'English',
+                                                                   'Reading', '1.jpeg', 'Leskova street,8',
+                                                                   'Uttar Pradesh Agra'))
+
+    # registration_page.should_include_info('Ivan Ivanov',
+    #                                       'ivanov@mail.com', 'Male', '8925239563',
+    #                                       '01 June,1988',
+    #                                       'English',
+    #                                       'Reading', '1.jpeg', 'Leskova street,8',
+    #                                       'Uttar Pradesh Agra')
